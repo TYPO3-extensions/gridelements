04475fc581e303a18dd20fcefd5262640fdef083
Tim Lochmüller <tim@fruit-lab.de>
2014-06-24 23:10:38 +0200

    [BUGFIX] Namespace class for hooks
    One hook has a invalid class name (without namespace). Fix it.
    
    Change-Id: I9625fa2f1b7f8282e9a39ffcd84a41d0e9b26134
    Reviewed-on: https://review.typo3.org/31116
    Reviewed-by: Jo Hasenau
    Tested-by: Jo Hasenau

--- Classes/Hooks/PreHeaderRenderHook.php
+++ Classes/Hooks/PreHeaderRenderHook.php
@@ -1,9 +1,13 @@
 <?php
+namespace GridElementsTeam\Gridelements\Hooks;
 
-class Tx_Gridelements_Hooks_PreHeaderRenderHook {
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+class PreHeaderRenderHook {
+
 	function main($arg) {
-		/** @var $pagerenderer t3lib_PageRenderer */
+		/** @var $pagerenderer \TYPO3\CMS\Core\Page\PageRenderer */
 		$pagerenderer = $arg['pageRenderer'];
-		$pagerenderer->addCssFile(t3lib_extMgm::extRelPath('gridelements') . 'Resources/Public/Backend/Css/Skin/t3skin_override.css');
+		$pagerenderer->addCssFile(ExtensionManagementUtility::extRelPath('gridelements') . 'Resources/Public/Backend/Css/Skin/t3skin_override.css');
 	}
 }
