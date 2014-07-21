1824f01414736595970f12f239fa76f4850288b3
Jo Hasenau <info@cybercraft.de>
2014-07-21 17:43:45 +0200

    Make the core patch fix work for Gridelements without rows as well
    Change-Id: If175c6fbfa926fa1827fe12e348d4bf6ab3db207

--- Classes/Hooks/DrawItem.php
+++ Classes/Hooks/DrawItem.php
@@ -110,9 +110,11 @@ class DrawItem implements PageLayoutViewDrawItemHookInterface {
 		}
 
 		// if there are any columns, lets build the content for them
+		$outerTtContentDataArray = $parentObject->tt_contentData['nextThree'];
 		if (count($colPosValues) > 0) {
 			$this->renderGridColumns($parentObject, $colPosValues, $gridContent, $gridElement, $editUidList, $singleColumn, $head, $showHidden, $deleteClause);
 		}
+		$parentObject->tt_contentData['nextThree'] = $outerTtContentDataArray;
 
 		// if we got a selected backend layout, we have to create the layout table now
 		if ($layoutUid && isset($layout['config'])) {
@@ -243,7 +245,6 @@ class DrawItem implements PageLayoutViewDrawItemHookInterface {
 	 * @return void
 	 */
 	public function renderGridColumns(PageLayoutView $parentObject, &$colPosValues, &$gridContent, &$row, &$editUidList, &$singleColumn, &$head, $showHidden, $deleteClause) {
-		$outerTtContentDataArray = $parentObject->tt_contentData['nextThree'];
 		foreach ($colPosValues as $colPos => $values) {
 			// first we have to create the column content separately for each column
 			// so we can check for the first and the last element to provide proper sorting
@@ -257,7 +258,6 @@ class DrawItem implements PageLayoutViewDrawItemHookInterface {
 			// we will need a header for each of the columns to activate mass editing for elements of that column
 			$this->setColumnHeader($parentObject, $head, $colPos, $values['name'], $editUidList);
 		}
-		$parentObject->tt_contentData['nextThree'] = $outerTtContentDataArray;
 	}
 
 	/**
