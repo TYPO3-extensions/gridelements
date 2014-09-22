7e93beae684465fa0413241c2a143901f2e30cf9
Jo Hasenau <info@cybercraft.de>
2014-09-22 20:09:02 +0200

    Changed layout field to varchar to enable alias names for layouts
    Change-Id: I6b816910cdf30d9e886a8d13fc6d967c4447ea28

--- ext_tables.sql
+++ ext_tables.sql
@@ -41,6 +41,7 @@ CREATE TABLE tx_gridelements_backend_layout (
 #
 CREATE TABLE tt_content (
 	colPos smallint(6) DEFAULT '0' NOT NULL,
+	layout varchar(255) DEFAULT '' NOT NULL,
 	backupColPos smallint(6) DEFAULT '-2' NOT NULL,
 	tx_gridelements_backend_layout varchar(255) DEFAULT '' NOT NULL,
 	tx_gridelements_children int(11) DEFAULT '0' NOT NULL,
