58512824bfb602330f0a8b498393eea9fb74b250
Jo Hasenau <info@cybercraft.de>
2015-04-17 11:18:13 +0200

    Updated version number
    Change-Id: Ieba949d5c9a45624951b54c0f88f962f31c8f357

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 It offers a lot of new features like advanced drag & drop or real references, that improve the usability of the page and list module to speed up the daily work with the backend.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '3.1.0',
+	'version' => '3.2.0',
 	'dependencies' => 'cms,version',
 	'conflicts' => 'templavoila,jfmulticontent',
 	'priority' => 'bottom',
@@ -38,7 +38,7 @@ It offers a lot of new features like advanced drag & drop or real references, th
 			'cms' => '',
 			'version' => '',
 			'php' => '5.3.7-0.0.0',
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(
 			'templavoila' => '',
