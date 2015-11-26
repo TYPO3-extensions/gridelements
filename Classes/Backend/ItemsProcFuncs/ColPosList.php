22e89953d118048dbfaffa5de74a88ba746294d9
Jo Hasenau <info@cybercraft.de>
2015-11-26 17:49:32 +0100

    [TASK] Disable possibility to "move" children by changing their colPos
    Change-Id: I5789465c6c3d8f164d474d0a4d6991fd9390a5e0
    Resolves: #66782
    Releases: 3-0
    Reviewed-on: https://review.typo3.org/44962
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- Classes/Backend/ItemsProcFuncs/ColPosList.php
+++ Classes/Backend/ItemsProcFuncs/ColPosList.php
@@ -43,13 +43,13 @@ class ColPosList extends AbstractItemsProcFunc {
 	 */
 	public function itemsProcFunc(&$params) {
 		if ($params['row']['pid'] > 0) {
-			$params['items'] = $this->addColPosListLayoutItems($params['row']['pid'], $params['items'], $params['row']['CType']);
+			$params['items'] = $this->addColPosListLayoutItems($params['row']['pid'], $params['items'], $params['row']['CType'], $params['row']['tx_gridelements_container']);
 		} else {
 			// negative uid_pid values indicate that the element has been inserted after an existing element
 			// so there is no pid to get the backendLayout for and we have to get that first
 			$existingElement = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('pid, CType', 'tt_content', 'uid=' . -((int)$params['row']['pid']));
 			if ($existingElement['pid'] > 0) {
-				$params['items'] = $this->addColPosListLayoutItems($existingElement['pid'], $params['items'], $existingElement['CType']);
+				$params['items'] = $this->addColPosListLayoutItems($existingElement['pid'], $params['items'], $existingElement['CType'], $existingElement['tx_gridelements_container']);
 			}
 		}
 	}
@@ -60,31 +60,37 @@ class ColPosList extends AbstractItemsProcFunc {
 	 * @param   integer $pageId : The uid of the page we are currently working on
 	 * @param   array $items : The array of items before the action
 	 * @param   string $CType : The content type of the item holding the colPosList
+	 * @param 	integer $container
 	 *
-	 * @return  array   $items: The ready made array of items
+	 * @return array $items: The ready made array of items
 	 */
-	protected function addColPosListLayoutItems($pageId, array $items, $CType = '') {
-		$layout = $this->getSelectedBackendLayout($pageId);
+	protected function addColPosListLayoutItems($pageId, array $items, $CType = '', $container = 0) {
+		if (empty($container)) {
+			$layout = $this->getSelectedBackendLayout($pageId);
 
-		if ($layout) {
-			if ($CType !== '' && count($layout['__items']) > 0) {
-				foreach ($layout['__items'] as $itemKey => $itemArray) {
-					if ($itemArray[3] !== '' && !GeneralUtility::inList($itemArray[3], $CType) && !GeneralUtility::inList($itemArray[3], '*')) {
-						unset($layout['__items'][$itemKey]);
+			if ($layout) {
+				if ($CType !== '' && count($layout['__items']) > 0) {
+					foreach ($layout['__items'] as $itemKey => $itemArray) {
+						if ($itemArray[3] !== '' && !GeneralUtility::inList($itemArray[3], $CType) && !GeneralUtility::inList($itemArray[3], '*')) {
+							unset($layout['__items'][$itemKey]);
+						}
 					}
 				}
+				if ($layout['__items'] && count($layout['__items']) > 0) {
+					$items = $layout['__items'];
+				}
 			}
-			if ($layout['__items'] && count($layout['__items']) > 0) {
-				$items = $layout['__items'];
-			}
+
+		} else {
+			$items = array();
+			$items[] = array(
+					$GLOBALS['LANG']->sL('LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xml:tt_content.tx_gridelements_container'),
+					'-1',
+					NULL,
+					NULL
+			);
 		}
 
-		$items[] = array(
-				$GLOBALS['LANG']->sL('LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xml:tt_content.tx_gridelements_container'),
-				'-1',
-				NULL,
-				NULL
-		);
 		return $items;
 	}
 }
