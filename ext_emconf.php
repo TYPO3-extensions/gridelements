322f48f9a6803b95a1fd934151fafccfa42875d4
Jo Hasenau <info@cybercraft.de>
2015-12-07 22:26:11 +0100

    [TASK] Raised version number and added change log entries
    Change-Id: If463c9afc6bccc972586e423488cd683297a9793

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 It offers a lot of new features like advanced drag & drop or real references, that improve the usability of the page and list module to speed up the daily work with the backend.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '3.3.1',
+	'version' => '3.3.2',
 	'dependencies' => 'cms',
 	'conflicts' => 'templavoila,jfmulticontent',
 	'priority' => 'bottom',
