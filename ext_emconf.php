21d1fb5b11fe9cdcd76ef68125f4bbb64538827d
Jo Hasenau <info@cybercraft.de>
2016-04-22 14:19:56 +0200

    [TASK] Changed version number to distinguish between TER version
    Change-Id: I1ebfa321a632791ad11b49e215652b2bce54ef02
    Reviewed-on: https://review.typo3.org/47856
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'description' => 'This extension integrates the grid layout concept also to regular content elements - the grid elements.
 It offers a lot of new features like advanced drag & drop or real references, that improve the usability of the page and list module to speed up the daily work with the backend.',
     'category' => 'be',
-    'version' => '7.0.5',
+    'version' => '7.0.6-dev',
     'priority' => 'bottom',
     'module' => '',
     'state' => 'beta',
