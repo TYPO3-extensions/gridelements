5d955c2f0b0ee2fe9a4bfc3e1f7302d222d95e7a
Cybercraft <info@cybercraft.de>
2017-12-18 11:20:36 +0100

    [BUGFIX] Use FQCN for classes included in ext_tables and ext_localconf
    Resolves: #83372
    Releases: master, 8-0
    Change-Id: I7fec66b7f0d07b2ef2fe743c2eee662c98426ed5
    (cherry picked from commit f703b78a5481339987d4729fc8d2d477a8a3987d)
    Reviewed-on: https://review.typo3.org/55146
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,5 @@
 <?php
 
-use GridElementsTeam\Gridelements\Slots\ExtTablesInclusionPostProcessing;
-use GridElementsTeam\Gridelements\Wizard\GridelementsBackendLayoutWizardElement;
-use GridElementsTeam\Gridelements\Xclass\DatabaseRecordList;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
 if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
@@ -16,21 +10,21 @@ if (TYPO3_MODE === 'BE') {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1488914437] = [
         'nodeName' => 'belayoutwizard',
         'priority' => 50,
-        'class'    => GridelementsBackendLayoutWizardElement::class,
+        'class'    => \GridElementsTeam\Gridelements\Wizard\GridelementsBackendLayoutWizardElement::class,
     ];
 
     // XCLASS
     if ($_EXTCONF['nestingInListModule']) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList::class] = ['className' => DatabaseRecordList::class];
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList::class] = ['className' => \GridElementsTeam\Gridelements\Xclass\DatabaseRecordList::class];
     }
 }
 
-ExtensionManagementUtility::addUserTSConfig('
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
 	options.saveDocNew.tx_gridelements_backend_layout=1
 ');
 
-ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Plugin/Gridelements.php', '_pi1',
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Plugin/Gridelements.php', '_pi1',
     'CType', 1);
 
-$signalSlotDispatcher = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-$signalSlotDispatcher->connect(ExtensionManagementUtility::class, 'tcaIsBeingBuilt', ExtTablesInclusionPostProcessing::class, 'processData');
+$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+$signalSlotDispatcher->connect(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::class, 'tcaIsBeingBuilt', \GridElementsTeam\Gridelements\Slots\ExtTablesInclusionPostProcessing::class, 'processData');
