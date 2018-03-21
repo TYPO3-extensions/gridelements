cee9e1de02a3e3a0bc657200b18499691c87bd07
Cybercraft <info@cybercraft.de>
2018-03-21 15:05:46 +0100

    [TASK] Raise version number
    Change-Id: I2b55dc17e11e7714d6a9e8a3c77b89ce539cbf43
    Reviewed-on: https://review.typo3.org/56411
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,10 +13,10 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'This extension integrates the grid layout concept also to regular content elements - the grid elements.
 It offers a lot of new features like advanced drag & drop or real references, that improve the usability of the page and list module to speed up the daily work with the backend.',
     'category' => 'be',
-    'version' => '8.0.0',
+    'version' => '8.1.0',
     'priority' => 'bottom',
     'module' => '',
-    'state' => 'beta',
+    'state' => 'stable',
     'uploadfolder' => true,
     'createDirs' => '',
     'modify_tables' => 'tt_content',
