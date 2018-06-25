ff416b6fbf14429d6e3dba81ba7dcca10489fb0b
Cybercraft <info@cybercraft.de>
2018-06-25 11:32:54 +0200

    [TASK] Add dependencies to make sure those are loaded before GE
    Change-Id: Ie5b0d2d134fa4733fd48a147cf94881e2d588179
    Resolves: #85314
    Releases: 7-0
    Reviewed-on: https://review.typo3.org/57365
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Grid Elements',
     'description' => 'This extension integrates the grid layout concept also to regular content elements - the grid elements.
 It offers a lot of new features like advanced drag & drop or real references, that improve the usability of the page and list module to speed up the daily work with the backend.',
@@ -16,7 +16,7 @@ It offers a lot of new features like advanced drag & drop or real references, th
     'version' => '7.4.2',
     'priority' => 'bottom',
     'module' => '',
-    'state' => 'beta',
+    'state' => 'stable',
     'uploadfolder' => true,
     'createDirs' => '',
     'modify_tables' => 'tt_content',
@@ -24,14 +24,18 @@ It offers a lot of new features like advanced drag & drop or real references, th
     'author' => 'Grid Elements Team',
     'author_email' => 'info@cybercraft.de',
     'author_company' => '',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '7.6.0-7.99.99',
-        ),
-        'conflicts' => array(
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-7.6.99',
+            'backend' => '7.6.0-7.6.99',
+            'recordlist' => '7.6.0-7.6.99',
+            'frontend' => '7.6.0-7.6.99',
+            'lang' => '7.6.0-7.6.99',
+        ],
+        'conflicts' => [
             'templavoila' => '',
-            'jfmulticontent' => '',
-        ),
-        'suggests' => array(),
-    ),
-);
+            'jfmulticontent' => ''
+        ],
+        'suggests' => [],
+    ],
+];
