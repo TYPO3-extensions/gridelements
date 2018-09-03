dc9476b374dffd0faa9b468262d866f23249eb06
cybercraft <info@cybercraft.de>
2018-09-03 22:12:36 +0200

    [FEATURE] Make use of the new records_is_used hook
    Change-Id: Ica57304fc5582ea448013a476596aa433e51d235
    Releases: master
    Resolves: #85636
    Reviewed-on: https://review.typo3.org/58161
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

---
+++ Classes/Hooks/PageLayoutView.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GridelementsTeam\Gridelements\Hooks;
+
+/***************************************************************
+ *  Copyright notice
+ *  (c) 2018 Jo Hasenau <info@cybercraft.de>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class PageLayoutView
+{
+    public function contentIsUsed(array $params, \TYPO3\CMS\Backend\View\PageLayoutView $parentObject): bool
+    {
+        if ($params['used']) {
+            return true;
+        }
+        $record = $params['record'];
+        return $record['colPos'] === -1 && !empty($record['tx_gridelements_container']);
+    }
+}
