a53b288a793d6735f244fda24246d73467179add
cybercraft <info@cybercraft.de>
2019-01-01 14:26:47 +0100

    [TASK] Remove superfluous plugin configuration
    Change-Id: I8e69baf6d41471397bde54b020039d12601c436e
    Reviewed-on: https://review.typo3.org/59316
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,9 +9,6 @@ $_EXTCONF = unserialize($_EXTCONF);
 	options.saveDocNew.tx_gridelements_backend_layout=1
 ');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Plugin/Gridelements.php', '_pi1',
-    'CType', 1);
-
 // XCLASS
 if ($_EXTCONF['nestingInListModule']) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Recordlist\\RecordList\\DatabaseRecordList'] = array('className' => 'GridElementsTeam\\Gridelements\\Xclass\\DatabaseRecordList',);
